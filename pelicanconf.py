449f1eeee7cc46271158d829a7e4892866fdbff7
Rae Knowler <rae.knowler@liip.ch>
2020-06-30 14:18:22 +0200

    Fix language selector

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,11 +14,11 @@ THEME = 'theme'
 CUSTOM_CSS = 'theme/ogdch/custom.css'
 
 STATIC_PATHS = [
-	'images', 'handbook/images',
-	'samples', 'export',
-	'../extra/robots.txt',
-	'../extra/favicon.ico',
-	'../extra/.htaccess'
+    'images', 'handbook/images',
+    'samples', 'export',
+    '../extra/robots.txt',
+    '../extra/favicon.ico',
+    '../extra/.htaccess'
 ]
 EXTRA_PATH_METADATA = {
     '../extra/robots.txt': {'path': 'robots.txt'},
@@ -26,7 +26,7 @@ EXTRA_PATH_METADATA = {
     '../extra/.htaccess': {'path': '.htaccess'}
 }
 
-ARTICLE_PATHS = ['handbook','library']
+ARTICLE_PATHS = ['handbook', 'library']
 ARTICLE_SAVE_AS = '{category}/{slug}.html'
 ARTICLE_URL = '{category}/{slug}.html'
 SHOW_ARTICLE_AUTHOR = True
@@ -65,27 +65,43 @@ PLUGINS = ['i18n_subsites', 'tipue_search', 'pelican-toc']
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
 I18N_SUBSITES = {
-	'en': {
-		'LOCALE': 'en_US',
-		'THEME_STATIC_DIR': 'theme'
-		},
-	'de': {
-		'LOCALE': 'de_CH',
-		'THEME_STATIC_DIR': 'theme'
-		},
-	'fr': {
-		'LOCALE': 'fr_CH',
-		'THEME_STATIC_DIR': 'theme'
-		},
-	'it': {
-		'LOCALE': 'it_IT',
-		'THEME_STATIC_DIR': 'theme'
-		}
+    'en': {
+        'LOCALE': 'en_US',
+        'THEME_STATIC_DIR': 'theme'
+    },
+    'de': {
+        'LOCALE': 'de_CH',
+        'THEME_STATIC_DIR': 'theme'
+    },
+    'fr': {
+        'LOCALE': 'fr_CH',
+        'THEME_STATIC_DIR': 'theme'
+    },
+    'it': {
+        'LOCALE': 'it_IT',
+        'THEME_STATIC_DIR': 'theme'
+    }
+}
+
+LANGUAGES_LOOKUP = {
+    'de': 'Deutsch',
+    'it': 'Italiano',
+    'fr': u'Français',
+    'en': 'English',
+}
+
+
+def lookup_lang_name(lang_code):
+    return LANGUAGES_LOOKUP[lang_code]
+
+
+JINJA_FILTERS = {
+    'lookup_lang_name': lookup_lang_name,
 }
 
 TOC = {
-    'TOC_HEADERS' : '^h[1-3]',  # What headers should be included in the generated toc
-    'TOC_RUN'     : 'false' # Default value for toc generation
+    'TOC_HEADERS': '^h[1-3]',  # What headers should be included in the generated toc
+    'TOC_RUN': 'false'  # Default value for toc generation
 }
 
 DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'authors', 'archives', 'search'))
